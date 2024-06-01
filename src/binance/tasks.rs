6eac30ed917040850f6cdf42e1bbc4f9d51bc811
GitButler <gitbutler@gitbutler.com>
2024-06-02 00:22:58 +0200

    GitButler Integration Commit
    This is an integration commit for the virtual branches that GitButler is tracking.
    
    Due to GitButler managing multiple virtual branches, you cannot switch back and
    forth between git branches and virtual branches easily. 
    
    If you switch to another branch, GitButler will need to be reinitialized.
    If you commit on this branch, GitButler will throw it away.
    
    Here are the branches that are currently applied:
     - test-push-1717279447809 (refs/gitbutler/test-push-1717279447809)
     - master (refs/gitbutler/master)
       branch head: 07bfb5695031616ea1a0292f376e0adfe52850a5
       - src/core/error.rs
       - src/binance/manager.rs
       - examples/btc-orderbook.rs
    For more information about what we're doing here, check out our docs:
    https://docs.gitbutler.com/features/virtual-branches/integration-branch

--- src/binance/tasks.rs
+++ src/binance/tasks.rs
@@ -3,31 +3,30 @@ use std::sync::Arc;
 
 use tokio::sync::oneshot;
 use tokio::sync::Mutex;
-use tokio::sync::Notify;
 
-use super::LocalBook;
+use crate::binance::LocalBook;
 use crate::Spot;
 
 pub enum Task {
     Subscribe(Spot, oneshot::Sender<Arc<Mutex<LocalBook>>>),
     SnapShot(Spot, oneshot::Sender<Arc<Mutex<LocalBook>>>),
-    Unsubscribe(Spot, Arc<Notify>),
+    Unsubscribe(Spot, oneshot::Sender<()>),
 }
 
 impl Task {
     pub fn subscribe(spot: Spot) -> (oneshot::Receiver<Arc<Mutex<LocalBook>>>, Self) {
-        let (sender, receiver) = oneshot::channel::<Arc<Mutex<LocalBook>>>();
+        let (sender, receiver) = oneshot::channel();
         (receiver, Task::Subscribe(spot, sender))
     }
 
     pub fn snapshot(spot: Spot) -> (oneshot::Receiver<Arc<Mutex<LocalBook>>>, Self) {
-        let (sender, receiver) = oneshot::channel::<Arc<Mutex<LocalBook>>>();
+        let (sender, receiver) = oneshot::channel();
         (receiver, Task::SnapShot(spot, sender))
     }
 
-    pub fn unsubscribe(spot: Spot) -> (Arc<Notify>, Self) {
-        let notify = Arc::new(Notify::new());
-        (notify.clone(), Task::Unsubscribe(spot, notify))
+    pub fn unsubscribe(spot: Spot) -> (oneshot::Receiver<()>, Self) {
+        let (sender, receiver) = oneshot::channel();
+        (receiver, Task::Unsubscribe(spot, sender))
     }
 }
 
